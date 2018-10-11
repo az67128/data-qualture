9e08257dda4736c415383636dc7aedc352d51a30
Alexander Zinchenko <az67128@gmail.com>
2018-10-11 14:33:22 +0300

    ololo
    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./component/App";
 import "./css/common.css";
 import { HashRouter } from "react-router-dom";
-
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <HashRouter>
